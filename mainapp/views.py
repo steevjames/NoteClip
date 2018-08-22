3be1e334535e89a572f4968e653927e72d35dcb0
Steev James <steevjames11@gmail.com>
2018-08-22 19:19:58 +0530

    Added Template

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Welcome to Noteclip</h1>")
+    return render(request, 'index.html', {})
