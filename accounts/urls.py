49517bd78d3e3d65d640fa8e45da1ab63de61a3d
Steev James <steevjames11@gmail.com>
2018-08-24 15:58:35 +0530

    Sign up is working

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,12 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from mainapp import views
 
-from django.views.generic.base import TemplateView
+
+from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('test', TemplateView.as_view(template_name='home.html'), name="home"),
+    path('signup', views.SignUp.as_view(), name='signup'),
 ]
