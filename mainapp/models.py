6d0795cc1885079c14d2d954266c193c6d0447ba
Steev James <steevjames11@gmail.com>
2018-08-25 12:07:12 +0530

    Model working properly

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class post(models.Model):
+class Note(models.Model):
     owner = models.CharField(max_length=50)
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=3000)
